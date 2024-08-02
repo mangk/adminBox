e6c09b0d44660259a52ce5e1d148145512187980
mangk <mangk.l@qq.com>
2024-08-02 16:55:00 +0800

    为用户名增加uniqueIndex；新增随机字符串创建方法

---
+++ utils/string.go
@@ -0,0 +1,17 @@
+package utils
+
+import (
+	"time"
+
+	"golang.org/x/exp/rand"
+)
+
+func RandString(n int) string {
+	letterBytes := "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+	rand.Seed(uint64(time.Now().UnixNano()))
+	b := make([]byte, n)
+	for i := range b {
+		b[i] = letterBytes[rand.Intn(len(letterBytes))]
+	}
+	return string(b)
+}
