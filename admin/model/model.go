06aae00f1d169e0b2b8b3f01e239f7591c6d94cd
mangk <mangk.l@qq.com>
2025-03-06 11:41:54 +0800

    更新LocalTim支持的方法

--- admin/model/model.go
+++ admin/model/model.go
@@ -100,6 +100,62 @@ func (Model) SQLQuery(driver, sql string) (data []map[string]interface{}, err er
 	return
 }
 
+func Find(tx *gorm.DB) (data []map[string]interface{}, err error) {
+	rows, err := tx.Rows()
+	if err != nil {
+		return
+	}
+	defer rows.Close()
+
+	columns, err := rows.Columns()
+	if err != nil {
+		return
+	}
+
+	// 列的个数
+	count := len(columns)
+
+	// 返回值 Map切片
+	data = make([]map[string]interface{}, 0)
+	// 一条数据的各列的值（需要指定长度为列的个数，以便获取地址）
+	values := make([]interface{}, count)
+	// 一条数据的各列的值的地址
+	valPointers := make([]interface{}, count)
+	for rows.Next() {
+
+		// 获取各列的值的地址
+		for i := 0; i < count; i++ {
+			valPointers[i] = &values[i]
+		}
+
+		// 获取各列的值，放到对应的地址中
+		rows.Scan(valPointers...)
+
+		// 一条数据的Map (列名和值的键值对)
+		entry := make(map[string]interface{})
+
+		// Map 赋值
+		for i, col := range columns {
+			var v interface{}
+
+			// 值复制给val(所以Scan时指定的地址可重复使用)
+			val := values[i]
+			b, ok := val.([]byte)
+			if ok {
+				// 字符切片转为字符串
+				v = string(b)
+			} else {
+				v = val
+			}
+			entry[col] = v
+		}
+
+		data = append(data, entry)
+	}
+
+	return
+}
+
 const (
 	TimeZone   = "Asia/Shanghai"
 	TimeFormat = "2006-01-02 15:04:05"
@@ -174,58 +230,14 @@ func (t LocalTime) Format(layout ...string) string {
 	return tTime.Format(TimeFormat)
 }
 
-func Find(tx *gorm.DB) (data []map[string]interface{}, err error) {
-	rows, err := tx.Rows()
-	if err != nil {
-		return
-	}
-	defer rows.Close()
+func (t LocalTime) Year() int {
+	return time.Time(t).Year()
+}
 
-	columns, err := rows.Columns()
-	if err != nil {
-		return
-	}
+func (t LocalTime) Month() time.Month {
+	return time.Time(t).Month()
+}
 
-	// 列的个数
-	count := len(columns)
-
-	// 返回值 Map切片
-	data = make([]map[string]interface{}, 0)
-	// 一条数据的各列的值（需要指定长度为列的个数，以便获取地址）
-	values := make([]interface{}, count)
-	// 一条数据的各列的值的地址
-	valPointers := make([]interface{}, count)
-	for rows.Next() {
-
-		// 获取各列的值的地址
-		for i := 0; i < count; i++ {
-			valPointers[i] = &values[i]
-		}
-
-		// 获取各列的值，放到对应的地址中
-		rows.Scan(valPointers...)
-
-		// 一条数据的Map (列名和值的键值对)
-		entry := make(map[string]interface{})
-
-		// Map 赋值
-		for i, col := range columns {
-			var v interface{}
-
-			// 值复制给val(所以Scan时指定的地址可重复使用)
-			val := values[i]
-			b, ok := val.([]byte)
-			if ok {
-				// 字符切片转为字符串
-				v = string(b)
-			} else {
-				v = val
-			}
-			entry[col] = v
-		}
-
-		data = append(data, entry)
-	}
-
-	return
+func (t LocalTime) Day() int {
+	return time.Time(t).Day()
 }
