46bae0e61f21ce4994b3f4ca8dd2d2a2ecace710
mangk <mangk.l@qq.com>
2025-07-07 10:48:02 +0800

    config更新

--- config/config.go
+++ config/config.go
@@ -35,6 +35,7 @@ func (c *configInstance) i() configInstance {
 	_configInitOnce.Do(func() {
 		if _configPath == "" { // 指定路径优先级最高
 			_configPath = filepath.Join(util.GetExecPath(), "config.yaml")
+			SetConfigPath(_configPath)
 			os.Chdir(_configPath)
 		}
 
