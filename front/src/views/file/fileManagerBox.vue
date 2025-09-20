ed496b73c7a0c0e5b13cdb776eea821189c85753
mangk <mangk.l@qq.com>
2025-09-20 23:02:51 +0800

    文件上传更新

--- front/src/views/file/fileManagerBox.vue
+++ front/src/views/file/fileManagerBox.vue
@@ -10,22 +10,24 @@
         </slot>
     </div>
 
-    <el-dialog v-model="show" title="Tips" width="500" :before-close="handleClose">
-        <span>This is a message</span>
-        <template #footer>
+    <el-dialog v-model="show" width="70vw" :show-close="false" :before-close="handleClose"
+        style="height: 70vh;background-color: #f1f2f5;">
+        <BaseManager style="width: calc(100% + 10px);height: calc(70vh - 50px);" />
+        <!-- <template #footer>
             <div class="dialog-footer">
                 <el-button @click="show = false">Cancel</el-button>
                 <el-button type="primary" @click="show = false">
                     Confirm
                 </el-button>
             </div>
-        </template>
+        </template> -->
     </el-dialog>
 
 </template>
 
 <script setup>
 import { ref } from "vue"
+import BaseManager from './baseManager.vue'
 
 const show = ref(true)
 
@@ -42,4 +44,12 @@ const handleClose = () => {
     show.value = false
 }
 </script>
-<style lang="css" module></style>
+<style lang="css">
+.el-dialog {
+    padding: 10px;
+}
+
+.el-dialog__header {
+    padding: 0;
+}
+</style>
