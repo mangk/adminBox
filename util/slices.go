72a25aae73d307434f7642fd74213a6c07ac35fc
mangk <mangk.l@qq.com>
2025-12-12 14:58:47 +0800

    新增公共方法

--- util/slices.go
+++ util/slices.go
@@ -1,5 +1,12 @@
 package util
 
+import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/tidwall/gjson"
+)
+
 func SlicesDistinct[T comparable](slice []T) []T {
 	// 使用 map 来记录已经出现的元素
 	seen := make(map[T]bool)
@@ -13,4 +20,25 @@ func SlicesDistinct[T comparable](slice []T) []T {
 	}
 
 	return result
+}
+
+func SliceColumn[T any](items []T, jsonPath string) (map[string][]T, error) {
+	result := make(map[string][]T)
+
+	for _, item := range items {
+		b, err := json.Marshal(item)
+		if err != nil {
+			return nil, err
+		}
+
+		key := gjson.GetBytes(b, jsonPath)
+		if !key.Exists() {
+			return nil, fmt.Errorf("json key '%s' not found", jsonPath)
+		}
+
+		k := key.String()
+		result[k] = append(result[k], item)
+	}
+
+	return result, nil
 }
