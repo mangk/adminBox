dfb6386c172a8f3d9c964ef4d3a53980ef446963
mangk <mangk.l@qq.com>
2026-04-20 18:03:50 +0800

    前端更新

--- pkg/admin/front/src/utils/loadUtil.js
+++ pkg/admin/front/src/utils/loadUtil.js
@@ -4,39 +4,30 @@ import { defineAsyncComponent, markRaw } from 'vue'
 import http from './requester'
 
 // 用一个对象来跟踪已加载的脚本及其回调队列
-const loadedScripts = {}
+const loadedScripts = {};
 export function loadJS(urls, callback = null) {
-  // 确保urls是一个数组
   if (typeof urls === 'string') {
     urls = [urls];
   }
 
-  // 创建一个Promise数组，用于并行加载所有脚本
-  const promises = urls.map((url) => new Promise((resolve, reject) => {
-    // 如果该脚本已加载过，直接跳过加载
+  const promises = urls.map(url => {
     if (loadedScripts[url]) {
-      resolve();
-      return;
+      return loadedScripts[url]; // 返回同一个Promise
     }
 
-    const script = document.createElement('script');
-    script.type = 'text/javascript';
-    script.src = url;
+    loadedScripts[url] = new Promise((resolve, reject) => {
+      const script = document.createElement('script');
+      script.src = url;
+      script.onload = resolve;
+      script.onerror = reject;
+      document.head.appendChild(script);
+    });
 
-    script.onload = resolve; // 脚本加载成功
-    script.onerror = reject; // 脚本加载失败
+    return loadedScripts[url];
+  });
 
-    document.head.appendChild(script);
-    loadedScripts[url] = true; // 标记脚本已加载
-  }));
-
-  // 所有脚本加载完毕后，执行回调
   Promise.all(promises).then(() => {
-    if (typeof callback === 'function') {
-      callback();
-    }
-  }).catch((error) => {
-    console.error('脚本加载失败:', error);
+    callback && callback();
   });
 }
 /**
